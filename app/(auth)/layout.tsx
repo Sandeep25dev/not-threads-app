c6954bd3e94b04f6ade546b78fc246658a88877a
Sandeep25dev <sandeepkumar250903@gmail.com>
2024-11-13 21:07:54 +0000

    project initialized with homepage completed

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import React from "react";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+export const metadata = {
+  title: "Not-Threads",
+  description: "A Threads-Clone app made using Next.js",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,39 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import React from "react";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import TopBar from "@/components/shared/TopBar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
+export const metadata = {
+  title: "Not-Threads",
+  description: "A Threads-Clone app made using Next.js",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <TopBar />
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
