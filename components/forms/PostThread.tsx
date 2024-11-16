13f467d944643c255a76211dff6d3c57d0f8129b
Sandeep25dev <sandeepkumar250903@gmail.com>
2024-11-16 21:37:48 +0000

    completed the PostThread component

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -67,7 +67,7 @@ function PostThread({ userId }: { userId: string }) {
           )}
         />
 
-        <Button type="submit" className="bg-primary-500">
+        <Button type="submit" className="bg-primary-500 text-light-1">
           Post Thread
         </Button>
       </form>
